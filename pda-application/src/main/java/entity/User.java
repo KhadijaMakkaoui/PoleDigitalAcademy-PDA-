90976777dd8c19fb6b174f50a85c83dcb997de99
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-01 14:58:04 +0100

    Add OneToMany relationship between role and user and map the two tables

--- pda-application/src/main/java/entity/User.java
+++ pda-application/src/main/java/entity/User.java
@@ -3,7 +3,7 @@ package entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +23,8 @@ public class User {
 
     @Column(name = "domaine", length = 150)
     private String domaine;
-
+   @ManyToOne()
+    Role role;
     public int getId_user() {
         return id_user;
     }
