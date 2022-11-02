fde8fbf63b16f6ea24a57479c8f31433c6bff843
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-02 11:38:39 +0100

    test adding an admin

--- pda-application/src/main/java/entity/App.java
+++ pda-application/src/main/java/entity/App.java
@@ -1,6 +1,7 @@
 package entity;
 
 import repository.implementation.activiteRepository;
+import repository.implementation.adminRepository;
 
 public class App {
     public static void main(String[] args) {
@@ -10,5 +11,12 @@ public class App {
         activiteRepository rep = new activiteRepository();
         rep.add(activite);
         System.out.println("added activite"+ activite.toString());
+
+        Admin admin = new Admin();
+        admin.setNom("khadija");
+
+        adminRepository repA = new adminRepository();
+        repA.add(admin);
+        System.out.println("added Admin"+ admin.toString());
     }
 }
