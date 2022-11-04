1607829ae6309e9030c9c1e5530137ae0ce882ba
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-11-04 08:43:13 +0100

    Resolve loginServlet problem we added a redirect to the homeservlet the redirect to the home.jsp

--- pda-application/src/main/java/entity/App.java
+++ pda-application/src/main/java/entity/App.java
@@ -2,25 +2,26 @@ package entity;
 
 import repository.implementation.activiteRepoImp;
 import repository.implementation.adminRepoImp;
+import service.implementation.adminServiceImp;
 
 public class App {
     public static void main(String[] args) {
-        Activite activite = new Activite();
-        activite.setTitre("testing");
+        //Activite activite = new Activite();
+        //activite.setTitre("testing");
 
-        activiteRepoImp rep = new activiteRepoImp();
-        rep.add(activite);
-        System.out.println("added activite"+ activite.toString());
+        //activiteRepoImp rep = new activiteRepoImp();
+        //rep.add(activite);
+        //System.out.println("added activite"+ activite.toString());
 
         Admin admin = new Admin();
         admin.setLogin("khadija");
         admin.setPassword("123");
+        adminServiceImp asi = new adminServiceImp();
+        asi.add(admin);
 
 
 
-        adminRepoImp repA = new adminRepoImp();
-        repA.add(admin);
-
-        System.out.println(repA.auth("khadija","123"));
+        boolean asicon = asi.auth("khadija", "123");
+        System.out.println(asicon);
     }
 }
