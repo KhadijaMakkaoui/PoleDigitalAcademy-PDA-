d419bf65704e8198881d53e310aadfb1e1d2cf17
Skyseras <yasserelmahfoudi@gmail.com>
2022-11-10 23:30:46 +0100

    delete done working on switch case update

--- pda-application/src/main/java/servlet/ExerciceServlet.java
+++ pda-application/src/main/java/servlet/ExerciceServlet.java
@@ -14,42 +14,97 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@WebServlet(name = "ExerciceServlet", value = "/exercice")
+@WebServlet(name = "ExerciceServlet", urlPatterns = {"/exercice", "/delete", "/update"})
 public class ExerciceServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+    	String action = request.getServletPath();
+    	RequestDispatcher dispatcher = null;
+        int id;
+        exerciceServiceImp ex = null;
+    	id = Integer.parseInt(request.getParameter("id"));
+    	ex = new exerciceServiceImp();
+    	ex.delete(ex.find(id));
+    	dispatcher = request.getRequestDispatcher("exercice.jsp");
+        dispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try {
-        	exerciceServiceImp ex = new exerciceServiceImp();
+    	String action = request.getServletPath();
+
+    	try {
             RequestDispatcher dispatcher = null;
-            Exercice exercice=new Exercice();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            int id;
+            exerciceServiceImp ex = null;
+            Exercice exercice = null;
+            SimpleDateFormat sdf = null;
+            String dateDeb = null;
+            Date debut = null;
 
-
-            String dateDeb = request.getParameter("dateDebut");
-            Date debut = sdf.parse(dateDeb);
-
-            String dateFin = request.getParameter("dateFin");
-            Date fin = sdf.parse(dateFin);
+            String dateFin = null;
+            Date fin = null;
             
-            String ann = request.getParameter("annee");
-            String stat = request.getParameter("status");
-            
-            exercice.setDate_debut(debut);
-            exercice.setDate_fin(fin);
-            exercice.setAnnee(ann);
-            exercice.setStatus(Status.valueOf(stat));
+            String ann = null;
+            String stat = null;
+            switch (action) {
+	            case "/exercice":
+	            	ex = new exerciceServiceImp();
+	                exercice=new Exercice();
+	                sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-            Exercice added = ex.add(exercice);
-            
-            dispatcher = request.getRequestDispatcher("exercice.jsp");
-            dispatcher.forward(request, response);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+	                dateDeb = request.getParameter("dateDebut");
+	                debut = sdf.parse(dateDeb);
+
+	                dateFin = request.getParameter("dateFin");
+	                fin = sdf.parse(dateFin);
+	                
+	                ann = request.getParameter("annee");
+	                stat = request.getParameter("status");
+	                
+	                exercice.setDate_debut(debut);
+	                exercice.setDate_fin(fin);
+	                exercice.setAnnee(ann);
+	                exercice.setStatus(Status.valueOf(stat));
+
+	                Exercice added = ex.add(exercice);
+	                
+	                dispatcher = request.getRequestDispatcher("exercice.jsp");
+	                dispatcher.forward(request, response);
+	                break;
+	             
+                case "/update":
+                    ex = new exerciceServiceImp();
+	                exercice=new Exercice();
+	                
+                	id = Integer.parseInt(request.getParameter("id"));
+
+	                sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+	                dateDeb = request.getParameter("dateDebut");
+	                debut = sdf.parse(dateDeb);
+
+	                dateFin = request.getParameter("dateFin");
+	                fin = sdf.parse(dateFin);
+	                
+	                ann = request.getParameter("annee");
+	                stat = request.getParameter("status");
+	                
+	                exercice.setId_exercice(id);
+	                exercice.setDate_debut(debut);
+	                exercice.setDate_fin(fin);
+	                exercice.setAnnee(ann);
+	                exercice.setStatus(Status.valueOf(stat));
+
+	                Exercice updated = ex.update(exercice);
+	                
+	                dispatcher = request.getRequestDispatcher("exercice.jsp");
+	                dispatcher.forward(request, response);
+                    break;
+            }
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
     }
 }
